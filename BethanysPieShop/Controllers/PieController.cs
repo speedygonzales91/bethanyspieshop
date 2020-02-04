1786aa000583482dab0625d3022ec4994836cc24
Molnár Zsolt <Molnar.Zsolt@ArgonSoft.hu>
2020-02-04 08:07:57 +0100

    Add Controller, View and VM to display Pies

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PieListViewModel pieListViewModel = new PieListViewModel();
+            pieListViewModel.Pies = _pieRepository.AllPies;
+            pieListViewModel.CurrentCategory = "Cheese Cakes";
+
+            //ViewBag.CurrentCategory = "Cheese cakes";  --DO NOT USE ViewBag!!!!! ViewModel should be created instead!
+            //return View(_pieRepository.AllPies);
+
+            return View(pieListViewModel);
+        }
+
+    }
+}
